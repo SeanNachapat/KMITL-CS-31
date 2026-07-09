9572ea8245e2dd3fbbcf5a5d6fd6e734433b54d5
Nachapat Iamphuang <SeanNachapat@gmail.com>
2026-07-09 14:44:06 +0700

    Week02(feat): implement L2_IsPrimeInterface, benchmarker, and prime calculation analysis tools

---
+++ Year 2/1-2026/DSA_6006/Labs6006/Lab02/IsPrime0.java
@@ -0,0 +1,11 @@
+class IsPrime0 implements L2_IsPrimeInterface {
+    public boolean isPrime(int n) {
+        if (n == 1) return false;
+        if (n <= 3) return true;
+        int m = n / 2;
+        for (int i = 2; i <= m; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
---
+++ Year 2/1-2026/DSA_6006/Labs6006/Lab02/L2_IsPrimeInterface.java
@@ -0,0 +1,3 @@
+public interface L2_IsPrimeInterface {
+    boolean isPrime(int n);
+}
